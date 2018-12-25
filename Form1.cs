59377d7f0f17b5223d6d8f2ae2c7f670b1145e85
IVAN <griphone11@mail.ru>
2018-12-25 12:32:29 +0400

    Изменение кнопки

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication14
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+		Sender = null;
         }
 	private void pictureBox1_Click(object sender, EventArgs e)
         {
